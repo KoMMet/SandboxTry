1301fcbf5980d6503e7641fdb0bf4a804418774b
守 小室 <hipukon2@gmail.com>
2018-11-11 00:11:06 +0900

    first commit

--- SandboxTry/Program.cs
+++ SandboxTry/Program.cs
@@ -1,12 +1,79 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace SandboxTry
+namespace ThreadSafeCollection
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Log log = new Log(cts);
+            var t1 = Task.Run(() => { log.write();});
+            var t4 = Task.Run(() => { log.write();});
+            var t2 = Task.Run(() => { log.get();});
+            var t3 = Task.Run(() =>
+            {
+                if (Console.ReadKey().KeyChar == 'q')
+                {
+                    cts.Cancel();
+                }
+            });
+            Task.WaitAll(t1, t2, t3,t4);
+            cts.Dispose();
+            Console.ReadKey();
+        }
+    }
+
+    class Log
+    {
+        BlockingCollection<string> BC = new BlockingCollection<string>();
+        private CancellationTokenSource cts;
+        public Log(CancellationTokenSource cts)
+        {
+            this.cts = cts;
+        }
+
+        public void write()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var msg = i.ToString();
+                if (BC.TryAdd(msg))
+                {
+                    Console.WriteLine("add suc " + msg);
+                }
+                else
+                {
+                    Console.WriteLine("add fail " + msg);
+                }
+            }
+            
+
+
+        }
+
+        public void get()
+        {
+            while (true)
+            {
+                if (BC.TryTake(out string msg))
+                {
+                    Console.WriteLine("take suc " + msg);
+                }
+                else
+                {
+                    Console.WriteLine("take fail " + msg);
+                }
+
+                if (cts.IsCancellationRequested)
+                {
+                    break;
+                }
+            }
+            
         }
     }
 }
