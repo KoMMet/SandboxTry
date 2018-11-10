0053102243572580235ef311029a544fcf01fc07
守 小室 <hipukon2@gmail.com>
2018-11-10 20:42:52 +0900

    プロジェクト ファイルを追加します。

---
+++ SandboxTry/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SandboxTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
